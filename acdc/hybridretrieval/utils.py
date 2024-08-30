6a86dbd02ba8a0725213e78931dbb156ce1813c7
Iustin Grigoras <iustin.grigoras@gmail.com>
2024-08-30 09:52:55 +0300

    Update utils

--- acdc/hybridretrieval/utils.py
+++ acdc/hybridretrieval/utils.py
@@ -26,7 +26,7 @@ import IPython
 from acdc.acdc_utils import MatchNLLMetric, frac_correct_metric, logit_diff_metric, kl_divergence, negative_log_probs
 import torch
 from acdc.docstring.utils import AllDataThings
-from acdc.hybridretrieval.datasets.kbicr_indirect import HybridRetrievalDataset  # NOTE: the only import that is different from the original code is for the task
+from acdc.hybridretrieval.datasets.kbicr_template_indirect import KBICRDataset
 from tqdm import tqdm
 import wandb
 from transformer_lens.HookedTransformer import HookedTransformer
@@ -62,7 +62,7 @@ def get_hybrid_retrieval_gpt2_small(device="cuda"):
 def get_model(device):
     tl_model = HookedTransformer.from_pretrained(
         "gpt2-small",  # load gpt-2 small
-        center_writing_weights=False,  # these are needed as this model is a Shortformer; this is a technical detail
+        center_writing_weights=False,
         center_unembed=False,
         fold_ln=False,
         device=device,
@@ -75,40 +75,38 @@ def get_model(device):
 
 def get_all_hybrid_retrieval_things(num_examples, device, metric_name, kl_return_one_element=True):
     tl_model = get_gpt2_small(device=device)
-    hybrid_retrieval_dataset = HybridRetrievalDataset()
-    
-    # Get datasets
-    clean_data, corrupted_data, clean_labels, clean_wrong_labels = hybrid_retrieval_dataset.get_dataset()
-    clean_data = clean_data.to(device)
-    corrupted_data = corrupted_data.to(device)
+    dataset = KBICRDataset(N=num_examples, seed=42)
 
+    seq_len = dataset.toks.shape[1]
+
+    clean_prompts = dataset.toks.long()[:num_examples, :seq_len - 1].to(device)
+    corrupted_prompts = dataset.toks.long()[num_examples:, :seq_len - 1].to(device) 
+    clean_prompts_correct_answers = dataset.toks.long()[:num_examples, seq_len - 1]
+    clean_prompts_wrong_answers = torch.as_tensor(dataset.non_s_tokenIDs[:num_examples], dtype=torch.long)
+
+    assert torch.equal(clean_prompts_correct_answers, torch.as_tensor(dataset.s_tokenIDs, dtype=torch.long))
+    assert torch.equal(clean_prompts_wrong_answers, torch.as_tensor(dataset.non_s_tokenIDs, dtype=torch.long))
+    
+    clean_prompts_correct_answers = clean_prompts_correct_answers.to(device)
+    clean_prompts_wrong_answers = clean_prompts_wrong_answers.to(device)
+    
     # Print the tensor for verification
-    print("Clean Data Datasets:")
-    print(clean_data.shape)
+    print(f"\nClean Data Shape: {clean_prompts.shape}")
+    print(f"\nCorrupted Data Shape: {corrupted_prompts.shape}")
+    print(f"\nClean Labels shape: {clean_prompts_correct_answers.shape}")
+    print(f"\nClean Wrong Labels shape:{clean_prompts_wrong_answers.shape}")
 
-    print("\nCorrupted Data Datasets:")
-    print(corrupted_data.shape)
-    
-    print("\nClean Labels shape:")
-    print(clean_labels.shape)
-
-    print("\nClean Wrong Labels shape:")
-    print(clean_wrong_labels.shape)
-
-    num_examples = clean_data.size(0)
-
-    # Define the split
-    validation_slice = num_examples // 2  # Half of the data for validation
+    validation_slice = num_examples // 2  
 
     # Split the data
-    validation_data = clean_data[:validation_slice, :]
-    validation_labels = clean_labels[:validation_slice]
-    validation_patch_data = corrupted_data[:validation_slice, :]
-    validation_wrong_labels = clean_wrong_labels[:validation_slice]
-    test_data = clean_data[validation_slice:, :]
-    test_labels = clean_labels[validation_slice:]
-    test_patch_data = corrupted_data[validation_slice:, :]
-    test_wrong_labels = clean_wrong_labels[validation_slice:]
+    validation_data = clean_prompts[:validation_slice, :]
+    validation_labels = clean_prompts_correct_answers[:validation_slice]
+    validation_patch_data = corrupted_prompts[:validation_slice, :]
+    validation_wrong_labels = clean_prompts_wrong_answers[:validation_slice]
+    test_data = clean_prompts[validation_slice:, :]
+    test_labels = clean_prompts_correct_answers[validation_slice:]
+    test_patch_data = corrupted_prompts[validation_slice:, :]
+    test_wrong_labels = clean_prompts_wrong_answers[validation_slice:]
 
     # Checking the shapes of the resulting datasets
     print(f"Shape of validation_data: {validation_data.shape}")
@@ -117,8 +115,8 @@ def get_all_hybrid_retrieval_things(num_examples, device, metric_name, kl_return
     print(f"Shape of test_patch_data: {test_patch_data.shape}")
 
     with torch.no_grad():
-        base_model_logits = tl_model(clean_data)[:, -1, :]
-        base_model_logprobs = F.log_softmax(base_model_logits, dim=-1)
+        base_model_logits = tl_model(clean_prompts)[:, -1, :]
+        base_model_logprobs = F.log_softmax(base_model_logits, dim=-1) # log_softmax results in negative values, because the logarithm of a no. < 1 is negative
 
     base_validation_logprobs = base_model_logprobs[:validation_slice, :]
     base_test_logprobs = base_model_logprobs[validation_slice:, :]
@@ -126,8 +124,8 @@ def get_all_hybrid_retrieval_things(num_examples, device, metric_name, kl_return
     if metric_name == "logit_diff":
         validation_metric = partial(
                 logit_diff_metric,
-                correct_labels=validation_labels, # put none here instead of validation_labels
-                wrong_labels=validation_wrong_labels, # put none here instead of validation_wrong_labels
+                correct_labels=validation_labels, 
+                wrong_labels=validation_wrong_labels,
             )
     elif metric_name == "kl_div":
         validation_metric = partial(
@@ -149,8 +147,8 @@ def get_all_hybrid_retrieval_things(num_examples, device, metric_name, kl_return
         ),
         "logit_diff": partial(
             logit_diff_metric,
-            correct_labels=test_labels, # put none here instead of test_labels,
-            wrong_labels=test_wrong_labels, # put none here instead of test_wrong_labels,
+            correct_labels=test_labels, 
+            wrong_labels=test_wrong_labels, 
         )
     }
     
@@ -159,31 +157,27 @@ def get_all_hybrid_retrieval_things(num_examples, device, metric_name, kl_return
         validation_data=validation_data,
         validation_metric=validation_metric,
         validation_labels=validation_labels, 
-        validation_wrong_labels=validation_wrong_labels, # put none here instead of validation_labels
+        validation_wrong_labels=validation_wrong_labels, 
         validation_mask=None,
         validation_patch_data=validation_patch_data,
         test_metrics=test_metrics,
         test_data=test_data,
-        test_labels=test_labels, # put none here instead of test)labels
+        test_labels=test_labels, 
         test_wrong_labels=test_wrong_labels, 
         test_mask=None,
         test_patch_data=test_patch_data
     )
 
-def main():
-    num_examples = 20
-    device = "cuda"
-    metric_name = "kl_div"
-    kl_return_one_element = True
+# def main():
+#     num_examples = 20
+#     device = "cuda"
+#     metric_name = "kl_div"
+#     kl_return_one_element = True
     
-    # Call the function to get all hybrid retrieval things
-    all_data_things = get_all_hybrid_retrieval_things(num_examples, device, metric_name, kl_return_one_element)
+#     all_data_things = get_all_hybrid_retrieval_things(num_examples, device, metric_name, kl_return_one_element)
     
-    # Print the returned data
-    print("All Data Things:")
-    print(all_data_things)
+#     print("All Data Things:")
+#     print(all_data_things)
     
-    # Perform further operations or tests using the returned data
-    
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
